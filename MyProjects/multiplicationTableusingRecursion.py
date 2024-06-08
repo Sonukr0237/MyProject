89090c9d3b663ad2da479b81c32322742d2b5732
admin <sk460819@gmail.com>
2024-06-08 18:15:14 +0530

    multiplication table using recursion.

--- MyProjects/multiplicationTableusingRecursion.py
+++ MyProjects/multiplicationTableusingRecursion.py
@@ -1,1 +1,21 @@
 # Print multiplication table of user input number by using recursion.
+
+
+def multiplicationTable(number, multiplier = 1, limit = 10):
+    if multiplier > limit:
+        return
+
+    print(f"{number} * {multiplier} = {number * multiplier}")
+
+    multiplicationTable(number, multiplier + 1, limit)
+
+def onlyNumericValue(prompt = "Enter a num :"):
+    while True:
+        try:
+            num = int(input(prompt))
+            return num
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+num = onlyNumericValue()
+multiplicationTable(num)
