e91d980da74cb5ad5c224f66668d0e439ec07de2
admin <sk460819@gmail.com>
2024-06-09 10:50:12 +0530

    Count lines from a file

---
+++ MyProjects/FHCountNumberofLines.py
@@ -0,0 +1,17 @@
+# 5.Write a Python program to count the number of lines in a text file.
+
+def countLineinFiles(filePath):
+
+    with open(filePath, 'r') as file:
+        lines = file.readlines()
+
+    numberofLines = len(lines)
+
+    return numberofLines
+
+
+filePath = r'C:\Users\admin\OneDrive\Desktop\testText.txt'
+
+lineCount = countLineinFiles(filePath)
+
+print("The count of lines in file is :", lineCount)
